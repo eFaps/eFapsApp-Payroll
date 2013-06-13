f7877e4632bafd421bc1f1e9641ecd5a8fc3cf64
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-06-13 21:52:51 +0000

    - payroll: adding util package
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/payroll/trunk@9614 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/payroll/util/Payroll.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.payroll.util;
+
+import java.util.UUID;
+
+import org.efaps.admin.common.SystemConfiguration;
+import org.efaps.util.cache.CacheReloadException;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+public final class Payroll
+{
+    /**
+     * Singelton.
+     */
+    private Payroll()
+    {
+    }
+
+    /**
+     * @return the SystemConfigruation for Payroll
+     * @throws CacheReloadException on error
+     */
+    public static SystemConfiguration getSysConfig()
+        throws CacheReloadException
+    {
+        // Payroll-Configuration
+        return SystemConfiguration.get(UUID.fromString("6f21b777-3c7d-4792-b3c0-8bfb6af0bf5e"));
+    }
+}
