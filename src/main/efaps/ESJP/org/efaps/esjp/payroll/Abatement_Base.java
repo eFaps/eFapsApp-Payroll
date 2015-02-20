31fe80e32ccd66b0ce3de5d041f9f71862bc71f0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2015-02-20 23:35:28 +0000

    - payroll: adding datamodel and basic UserInterface for alterations
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/payroll/trunk@14891 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/payroll/Abatement_Base.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.payroll;
+
+import java.math.BigDecimal;
+
+import org.efaps.admin.event.Parameter;
+import org.efaps.admin.event.Return;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
+import org.efaps.esjp.ci.CIERP;
+import org.efaps.esjp.ci.CIFormPayroll;
+import org.efaps.esjp.ci.CIPayroll;
+import org.efaps.esjp.common.uiform.Create;
+import org.efaps.esjp.erp.Currency;
+import org.efaps.esjp.sales.document.AbstractDocument;
+import org.efaps.util.EFapsException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id: $
+ */
+@EFapsUUID("87300c22-90b2-4997-8953-e4b451731751")
+@EFapsRevision("$Rev: 14839 $")
+public abstract class Abatement_Base
+    extends AbstractDocument
+{
+
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Create create = new Create()
+        {
+
+            @Override
+            protected void add2basicInsert(final Parameter _parameter,
+                                           final Insert _insert)
+                throws EFapsException
+            {
+                super.add2basicInsert(_parameter, _insert);
+                _insert.add(CIERP.DocumentAbstract.Name, getDocName4Create(_parameter));
+                _insert.add(CIERP.DocumentAbstract.Name, getDocName4Create(_parameter));
+                _insert.add(CIPayroll.DocumentAbstract.RateCrossTotal, BigDecimal.ZERO);
+                _insert.add(CIPayroll.DocumentAbstract.RateNetTotal, BigDecimal.ZERO);
+                _insert.add(CIPayroll.DocumentAbstract.Rate, new Object[] { BigDecimal.ZERO, BigDecimal.ZERO });
+                _insert.add(CIPayroll.DocumentAbstract.CrossTotal, BigDecimal.ZERO);
+                _insert.add(CIPayroll.DocumentAbstract.NetTotal, BigDecimal.ZERO);
+                _insert.add(CIPayroll.DocumentAbstract.DiscountTotal, 0);
+                _insert.add(CIPayroll.DocumentAbstract.RateDiscountTotal, 0);
+                _insert.add(CIPayroll.DocumentAbstract.CurrencyId, Currency.getBaseCurrency());
+                _insert.add(CIPayroll.DocumentAbstract.RateCurrencyId, Currency.getBaseCurrency());
+                _insert.add(CIPayroll.DocumentAbstract.EmployeeAbstractLink, Instance.get(
+                            _parameter.getParameterValue(CIFormPayroll.Payroll_AlterationAbatementForm.employee.name)));
+            }
+        };
+        return create.execute(_parameter);
+    }
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/payroll/Increment.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2003 - 2015 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.payroll;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * This class must be replaced for customization, therefore it is left empty.
+ * Functional description can be found in the related "<code>_Base</code>"
+ * class.
+ *
+ * @author The eFaps Team
+ * @version $Id: Payslip.java 14839 2015-02-11 01:49:12Z jan@moxter.net $
+ */
+@EFapsUUID("ef7f78c8-33dd-401c-a562-b2520358d7ff")
+@EFapsRevision("$Rev: 14839 $")
+public class Increment
+    extends Increment_Base
+{
+
+}
